9578fe1bbc96f38d28a5f6ec568d7ea021d0a8aa
Andrew Hunt <andrew.e.hunt@gmail.com>
2013-07-09 23:33:05 +0000

    buffer, not Buffer\!

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     
     var buffer = fs.readFileSync('index.html');
 
-    response.send (Buffer.toString('utf-8'));
+    response.send (buffer.toString('utf-8'));
 
 });
 
