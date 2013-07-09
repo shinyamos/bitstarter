add3fcca49af3f225e33b4bc62cd1863c534034e
Andrew Hunt <andrew.e.hunt@gmail.com>
2013-07-09 00:44:14 +0000

    first attempt at hw3-1

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var textBuffer = new Buffer();
+    
+    var byteLength = fs.readSync('index.html', textBuffer);
+
+    response.send (textBuffer.toString('utf-8'));
+
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
